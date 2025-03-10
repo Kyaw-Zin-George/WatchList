7958c986bc6d054099b173905bd17021a3e04a62
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-10 16:44:34 +0000

    Initial Commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Kyaw Thant Zin(George) on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
