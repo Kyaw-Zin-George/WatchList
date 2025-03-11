a27ae4d858f1c6a2d5a19ca4ffc9c41ed33f4b76
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-11 12:10:57 +0000

    Creating  a pagetab view

---
+++ WatchList/Components/PageTabView.swift
@@ -0,0 +1,29 @@
+//
+//  PageTabView.swift
+//  WatchList
+//
+//  Created by Kyaw Thant Zin(George) on 3/11/25.
+//
+
+import SwiftUI
+
+struct PageTabView: View {
+    var icon: String
+    var description: String
+    var body: some View {
+        VStack{
+            Image(systemName: icon)
+                .imageScale(.large)
+                .font(.largeTitle)
+                .symbolEffect(.breathe)
+            
+            Text(description)
+                .font(.title.weight(.light))
+                .fontWidth(.compressed)
+        }
+    }
+}
+
+#Preview {
+    PageTabView(icon: "1.circle", description: "Add some favorite movies to watch")
+}
