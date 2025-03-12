0d19d2642667e690644d5321bd11c45573214264
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-12 23:46:26 +0000

    Adding Title a textfield and a genre selection picker for the app

---
+++ WatchList/Screens/NewMovieFormView.swift
@@ -0,0 +1,41 @@
+//
+//  NewMovieFormView.swift
+//  WatchList
+//
+//  Created by Kyaw Thant Zin(George) on 3/12/25.
+//
+
+import SwiftUI
+
+struct NewMovieFormView: View {
+    //MARK: PROPERTIES
+    @State private var title: String = ""
+    @State private var selectedGenre: Genre = .kids
+    
+    var body: some View {
+        List {
+            //MARK: HEADER
+            Text("What To Watch")
+                .font(.largeTitle.weight(.black))
+                .foregroundStyle(.blue)
+                .frame(minWidth: 0,maxWidth: .infinity,alignment: .center)
+                .padding(.vertical)
+            //MARK: TITLE
+            TextField("Movie Title", text: $title)
+                .textFieldStyle(.roundedBorder)
+                .font(.largeTitle.weight(.light))
+             //MARK: GENRE
+            Picker("Genre", selection: $selectedGenre) {
+                ForEach(Genre.allCases, id: \.self) { genre in
+                    Text(genre.name)
+                        .tag(genre)
+                }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    NewMovieFormView()
+}
