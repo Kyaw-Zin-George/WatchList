a45c054c48170fe9cc02e7c6cdcd94d67f237450
Kyaw-Zin-George <ktz1695@gmail.com>
2025-03-13 18:50:45 +0000

    Adding necessary labels and change ui design for user experience

--- WatchList/Screens/ContentView.swift
+++ WatchList/Screens/ContentView.swift
@@ -13,19 +13,46 @@ struct ContentView: View {
     @State private var isSheetPresented: Bool = false
     var body: some View {
         List{
-            ForEach(movies){ movie in
-                HStack{
-                    Text(movie.title)
-                    Spacer()
-                    Text(movie.genre.name)
-                }
-                .swipeActions {
-                    Button(role: .destructive){
-                        withAnimation {
-                            modelContext.delete(movie)
+            if !movies.isEmpty {
+                Section(
+                    header:
+                        VStack{
+                            Text("WatchList")
+                                .font(.largeTitle.weight(.black))
+                                .foregroundStyle(.blue.gradient)
+                                .padding()
+                            HStack{
+                                Label("Title", systemImage: "movieclapper")
+                                Spacer()
+                                Label("Genre",systemImage: "tag")
+                            }
                         }
-                    }label: {
-                        Label("Delete", systemImage: "trash")
+                ) {
+                    ForEach(movies){ movie in
+                        HStack{
+                            Text(movie.title)
+                                .font(.title.weight(.light))
+                                .padding(.vertical,2)
+                            Spacer()
+                            Text(movie.genre.name)
+                                .font(.footnote.weight(.medium))
+                                .padding(.horizontal,6)
+                                .padding(.vertical,3)
+                                .background(
+                                    Capsule()
+                                        .stroke(lineWidth: 1)
+                                )
+                                .foregroundStyle(.tertiary)
+                        }
+                        .swipeActions {
+                            Button(role: .destructive){
+                                withAnimation {
+                                    modelContext.delete(movie)
+                                }
+                            }label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
                     }
                 }
             }
